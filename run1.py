dbba07dc7b6b3fdf08dee50144222eb278b8eab7
tmayer1010 <118377813+tmayer1010@users.noreply.github.com>
2025-04-10 11:16:55 -0500

    Update run1.py

--- run1.py
+++ run1.py
@@ -25,7 +25,8 @@ if app_mode=='Home':
 	st.markdown('Dataset :')    
 	data=pd.read_csv('test.csv')    
 	st.write(data.head())    
-st.markdown('Applicant Income VS Loan Amount ')   st.bar_chart(data[['ApplicantIncome','LoanAmount']].head(20))
+st.markdown('Applicant Income VS Loan Amount ')   
+st.bar_chart(data[['ApplicantIncome','LoanAmount']].head(20))
  
    
 elif app_mode == 'Prediction':     
@@ -34,8 +35,18 @@ elif app_mode == 'Prediction':
 	gender_dict = {"Male":1,"Female":2}    
 	feature_dict = {"No":1,"Yes":2}    
 	edu={'Graduate':1,'Not Graduate':2}    
-prop={'Rural':1,'Urban':2,'Semiurban':3}    ApplicantIncome=st.sidebar.slider('ApplicantIncome',0,10000,0,)    CoapplicantIncome=st.sidebar.slider('CoapplicantIncome',0,10000,0,)    LoanAmount=st.sidebar.slider('LoanAmount in K$',9.0,700.0,200.0)    Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,40.0,300.0,360.0))    
-Credit_History=st.sidebar.radio('Credit_History',(0.0,1.0))    Gender=st.sidebar.radio('Gender',tuple(gender_dict.keys()))    Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))    Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))    Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])    Education=st.sidebar.radio('Education',tuple(edu.keys()))    Property_Area=st.sidebar.radio('Property_Area',tuple(prop.keys()))    
+	prop={'Rural':1,'Urban':2,'Semiurban':3}    
+	ApplicantIncome=st.sidebar.slider('ApplicantIncome',0,10000,0,)    
+	CoapplicantIncome=st.sidebar.slider('CoapplicantIncome',0,10000,0,)   
+	LoanAmount=st.sidebar.slider('LoanAmount in K$',9.0,700.0,200.0)    
+	Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,40.0,300.0,360.0))    
+	Credit_History=st.sidebar.radio('Credit_History',(0.0,1.0))    
+	Gender=st.sidebar.radio('Gender',tuple(gender_dict.keys()))   
+	Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))   
+	Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))    
+	Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])    
+	Education=st.sidebar.radio('Education',tuple(edu.keys()))    
+	Property_Area=st.sidebar.radio('Property_Area',tuple(prop.keys()))    
 
 class_0 , class_3 , class_1,class_2 = 0,0,0,0    
 if Dependents == '0':        
